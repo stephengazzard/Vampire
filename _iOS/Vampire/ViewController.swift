1fdd8ab584558d8c89fc27fa9fd02c212dd1d0d6
stephengazzard <stephen.gazzard@gmail.com>
2015-06-11 12:46:35 -0700

    Initial Project created

---
+++ _iOS/Vampire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vampire
+//
+//  Created by Stephen Gazzard on 2015-06-11.
+//  Copyright © 2015 Broken Kings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

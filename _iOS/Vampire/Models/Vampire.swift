9628b1f01dfd5ddbc6bd6751d4d340932ef12435
stephengazzard <stephen.gazzard@gmail.com>
2015-06-11 13:02:20 -0700

    Create Vampire model

---
+++ _iOS/Vampire/Models/Vampire.swift
@@ -0,0 +1,13 @@
+//
+//  Vampire.swift
+//  Vampire
+//
+//  Created by Stephen Gazzard on 2015-06-11.
+//  Copyright © 2015 Broken Kings. All rights reserved.
+//
+
+import Foundation
+
+class Vampire: NSObject {
+    var hunger : Float = 0
+}
